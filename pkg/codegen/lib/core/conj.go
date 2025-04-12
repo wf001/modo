fcfb7cf6cf4af229df8dcdbd6fa6970c21d4097a
wf001 <wf001@diax.xyz>
2025-04-12 14:52:38 +0900

    refactor to remove duplication in vector operation

--- pkg/codegen/lib/core/conj.go
+++ pkg/codegen/lib/core/conj.go
@@ -6,44 +6,31 @@ import (
 	"github.com/llir/llvm/ir/types"
 	"github.com/llir/llvm/ir/value"
 
+	"github.com/wf001/modo/pkg/codegen/lib/core/vector"
 	"github.com/wf001/modo/pkg/log"
 	mTypes "github.com/wf001/modo/pkg/types"
 )
 
 func InvokeConj(block *ir.Block, libs *mTypes.BuiltinLibProp, node *mTypes.Node) value.Value {
-	value, ok := node.IRValue.(*ir.InstAlloca)
+	oldArrPtr, ok := node.IRValue.(*ir.InstAlloca)
 	if !ok {
-		log.Panic("Array elements must be ir.InstAlloca: have %+v", value)
-	}
-	t := value.ElemType.(*types.ArrayType)
-	arrType := types.NewArray(t.Len+1, t.ElemType)
-	var newArr = block.NewAlloca(arrType)
-
-	for i := uint64(0); i < t.Len; i++ {
-		oldElemPtr := block.NewGetElementPtr(
-			value.ElemType,
-			value,
-			constant.NewInt(types.I32, 0),
-			constant.NewInt(types.I32, int64(i)),
-		)
-		// Arrayの要素の型取得
-		elem := block.NewLoad(t.ElemType, oldElemPtr)
-		newElemPtr := block.NewGetElementPtr(
-			arrType,
-			newArr,
-			constant.NewInt(types.I32, 0),
-			constant.NewInt(types.I32, int64(i)),
-		)
-		block.NewStore(elem, newElemPtr)
+		log.Panic("Array elements must be ir.InstAlloca: have %+v", oldArrPtr)
 	}
 
+	newValue := node.Next.IRValue
+	oldArr := oldArrPtr.ElemType.(*types.ArrayType)
+
+	newArr := vector.CopyArray(block, oldArrPtr, oldArr.ElemType, oldArr.Len+1, oldArr.Len)
+
+	newArrType := types.NewArray(oldArr.Len, oldArr.ElemType)
 	newElemPtr := block.NewGetElementPtr(
-		arrType,
+		newArrType,
 		newArr,
 		constant.NewInt(types.I32, 0),
-		constant.NewInt(types.I32, int64(t.Len)),
+		constant.NewInt(types.I32, int64(oldArr.Len)),
 	)
 	block.NewStore(node.Next.IRValue, newElemPtr)
+	block.NewStore(newValue, newElemPtr)
 
 	return newArr
 }
---
+++ pkg/codegen/lib/core/vector/copy.go
@@ -0,0 +1,38 @@
+package vector
+
+import (
+	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/constant"
+	"github.com/llir/llvm/ir/types"
+)
+
+func CopyArray(
+	block *ir.Block,
+	oldArrPtr *ir.InstAlloca, // Pointer to the allocated memory of the original array
+	arrType types.Type, // The type of elements in the array (e.g., types.I32)
+	arrSize uint64, // The total size of the new array (number of elements to allocate)
+	elemSize uint64, // The number of elements to copy from the old array to the new one
+) *ir.InstAlloca {
+	newArrType := types.NewArray(arrSize, arrType)
+
+	var newArr = block.NewAlloca(newArrType)
+	for i := uint64(0); i < elemSize; i++ {
+		oldElemPtr := block.NewGetElementPtr(
+			oldArrPtr.ElemType,
+			oldArrPtr,
+			constant.NewInt(types.I32, 0),
+			constant.NewInt(types.I32, int64(i)),
+		)
+		// Arrayの要素の型取得
+		elem := block.NewLoad(arrType, oldElemPtr)
+		newElemPtr := block.NewGetElementPtr(
+			newArrType,
+			newArr,
+			constant.NewInt(types.I32, 0),
+			constant.NewInt(types.I32, int64(i)),
+		)
+		block.NewStore(elem, newElemPtr)
+	}
+	return newArr
+
+}

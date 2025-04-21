3fe4b3b1746f6ca1e8f05b5fa9bc2f3a7107c155
wf001 <wf001@diax.xyz>
2025-04-21 09:46:17 +0900

    remove ND_TYPE_REFERENCE

--- pkg/types/node.go
+++ pkg/types/node.go
@@ -17,17 +17,16 @@ const (
 	ND_PROGRAM_ROOT = NodeKind("ND_PROGRAM_ROOT")
 
 	// reserved symbol
-	ND_VAR_DECLARE    = NodeKind("ND_VAR_DECLARE")   // variables and functions
-	ND_VAR_REFERENCE  = NodeKind("ND_VAR_REFERENCE") // variables and functions
-	ND_TYPE_DECLARE   = NodeKind("ND_TYPE_DECLARE")
-	ND_TYPE_REFERENCE = NodeKind("ND_TYPE_REFERENCE")
-	ND_DECLARE        = NodeKind("ND_DECLARE") // def
-	ND_LAMBDA         = NodeKind("ND_LAMBDA")  // fn
-	ND_BIND           = NodeKind("ND_BIND")    // let
-	ND_EXPR           = NodeKind("ND_EXPR")    // set of functions
-	ND_IF             = NodeKind("ND_IF")      // if
-	ND_FUNCCALL       = NodeKind("ND_FUNCCALL")
-	ND_LIBCALL        = NodeKind("ND_LIBCALL")
+	ND_VAR_DECLARE   = NodeKind("ND_VAR_DECLARE")   // variables and functions
+	ND_VAR_REFERENCE = NodeKind("ND_VAR_REFERENCE") // variables and functions
+	ND_TYPE_DECLARE  = NodeKind("ND_TYPE_DECLARE")
+	ND_DECLARE       = NodeKind("ND_DECLARE") // def
+	ND_LAMBDA        = NodeKind("ND_LAMBDA")  // fn
+	ND_BIND          = NodeKind("ND_BIND")    // let
+	ND_EXPR          = NodeKind("ND_EXPR")    // set of functions
+	ND_IF            = NodeKind("ND_IF")      // if
+	ND_FUNCCALL      = NodeKind("ND_FUNCCALL")
+	ND_LIBCALL       = NodeKind("ND_LIBCALL")
 
 	// type
 	ND_SCALAR     = NodeKind("ND_SCALAR")     // int32, string, bool

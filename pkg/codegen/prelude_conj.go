dfcbe84e6b1fc6c94f6b15daa7d9dc29aef5d40f
wf001 <wf001@diax.xyz>
2025-04-23 09:54:22 +0900

    remove unused ConjOld, GetOld

--- pkg/codegen/prelude_conj.go
+++ pkg/codegen/prelude_conj.go
@@ -1,12 +1,10 @@
 package codegen
 
 import (
-	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
 	"github.com/llir/llvm/ir/value"
 
-	"github.com/wf001/modo/pkg/log"
 	mTypes "github.com/wf001/modo/pkg/types"
 )
 
@@ -45,28 +43,4 @@ func PreludeConj(ctx *Context, n *mTypes.Node) value.Value {
 	ctx.block.NewStore(newLen, newLenField)
 
 	return newStructAlloca
-}
-
-// Note: remain here until it will be defined the strategy of memory lifecycle
-func PreludeConjOld(block *ir.Block, internal *mTypes.Internal, node *mTypes.Node) value.Value {
-	oldArrPtr, ok := node.IRValue.(*ir.InstAlloca)
-	if !ok {
-		log.Panic("Array elements must be ir.InstAlloca: have %+v", oldArrPtr)
-	}
-
-	newValue := node.Next.IRValue
-	oldArr := oldArrPtr.ElemType.(*types.ArrayType)
-
-	newArr := CopyArrayOld(block, oldArrPtr, oldArr.ElemType, oldArr.Len+1, oldArr.Len)
-
-	newElemPtr := block.NewGetElementPtr(
-		newArr.ElemType,
-		newArr,
-		constant.NewInt(types.I32, 0),
-		constant.NewInt(types.I32, int64(oldArr.Len)),
-	)
-	block.NewStore(node.Next.IRValue, newElemPtr)
-	block.NewStore(newValue, newElemPtr)
-
-	return newArr
 }
--- pkg/codegen/prelude_get.go
+++ pkg/codegen/prelude_get.go
@@ -9,7 +9,6 @@ import (
 	"github.com/llir/llvm/ir/types"
 	"github.com/llir/llvm/ir/value"
 
-	"github.com/wf001/modo/pkg/log"
 	mTypes "github.com/wf001/modo/pkg/types"
 )
 
@@ -101,27 +100,4 @@ func PreludeGet(ctx *Context, n *mTypes.Node) value.Value {
 		}
 	}
 	return getVec(ctx, n)
-}
-
-// Note: remain here until it will be defined the strategy of memory lifecycle
-func PreludeGetOld(block *ir.Block, internal *mTypes.Internal, node *mTypes.Node) value.Value {
-	value, ok := node.IRValue.(*ir.InstAlloca)
-	if !ok {
-		log.Panic("Array elements must be ir.InstAlloca: have %+v", value)
-	}
-	t := value.ElemType.(*types.ArrayType)
-
-	i, _ := strconv.ParseInt(node.Next.Val, 10, 32)
-	if i >= int64(t.Len) {
-		log.Panic("Array index out of range: have %d but array length %d", i, node.Len)
-	}
-	elemPtr := block.NewGetElementPtr(
-		value.ElemType,
-		value,
-		constant.NewInt(types.I32, 0),
-		constant.NewInt(types.I32, i),
-	)
-	elem := block.NewLoad(t.ElemType, elemPtr)
-
-	return elem
 }

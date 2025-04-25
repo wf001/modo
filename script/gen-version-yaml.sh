654095fd2a03b7c176c5149873b35fdb8b90117f
wf001 <wf001@diax.xyz>
2025-04-25 21:55:57 +0900

    trim quote from version info file

--- script/gen-version-yaml.sh
+++ script/gen-version-yaml.sh
@@ -6,7 +6,7 @@ LLVM_ARCH=$(llvm-config --host-target)
 COMMIT_HASH=$(git rev-parse --short=7 HEAD)
 
 cat <<EOF > cmd/modo/version.yaml
-version: "$VERSION"
-arch: "$LLVM_ARCH"
-commit: "$COMMIT_HASH"
+version: $VERSION
+arch: $LLVM_ARCH
+commit: $COMMIT_HASH
 EOF

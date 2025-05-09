f59b1ca17a8678ffc397c022febcb602d5856b7a
wf001 <wf001@diax.xyz>
2025-05-09 15:24:50 +0900

    add walkNode function and use it at parser validator

--- pkg/parser/validator.go
+++ pkg/parser/validator.go
@@ -6,63 +6,6 @@ import (
 	mTypes "github.com/wf001/modo/pkg/types"
 )
 
-func validateVarDeclare(n *mTypes.Node, root *mTypes.Node, used map[string]bool) {
-	// To check for duplicate variable and type declarations, a Set would be appropriate,
-	// but since Go doesn't include one in the standard library,
-	// implement provisionally using a map to avoid extra dependencies.
-	if n == nil {
-		return
-	}
-
-	if n.IsKind(mTypes.ND_TYPE_DECLARE) {
-		used[n.Val] = true
-		return
-	}
-
-	if n.IsKind(mTypes.ND_VAR_DECLARE) {
-		if used[n.Val] {
-			log.Panic(
-				"%s: cannot use %s, already used",
-				error.ERROR_SYNTAX_ERROR,
-				n.Val,
-			)
-		}
-		used[n.Val] = true
-
-		if n.Child == nil {
-			log.Panic(
-				"%s: the value of '%s' not defined",
-				error.ERROR_SYNTAX_ERROR,
-				n.Val,
-			)
-
-		} else if n.Child.Type == nil {
-			// skip validating
-
-		} else if n.Child.IsKind(mTypes.ND_FUNCCALL) {
-			// skip validating
-
-		} else if !n.IsType(n.Child.Type.Value) {
-			log.Panic(
-				"%s: cannot use %s type as %s (%s type)",
-				error.ERROR_SYNTAX_ERROR,
-				n.Child.Type.Value,
-				n.Val,
-				n.Type.Value,
-			)
-		}
-	}
-	if n.Next != nil {
-		validateVarDeclare(n.Next, root, used)
-	}
-	if n.Child != nil {
-		validateVarDeclare(n.Child, root, used)
-	}
-	if n.Bind != nil {
-		validateVarDeclare(n.Bind, root, used)
-	}
-}
-
 func findDeclare(n *mTypes.Node, targetVal string) *mTypes.Node {
 	if n == nil {
 		return nil
@@ -85,21 +28,6 @@ func findDeclare(n *mTypes.Node, targetVal string) *mTypes.Node {
 	return nil
 }
 
-func validateReference(n *mTypes.Node, root *mTypes.Node) {
-	if n == nil {
-		return
-	}
-	if n.IsKind(mTypes.ND_VAR_REFERENCE) {
-		decl := findDeclare(root, n.Val)
-		if decl == nil {
-			log.Panic("%s: undefined: %s", error.ERROR_SYNTAX_ERROR, n.Val)
-		}
-	}
-	validateReference(n.Next, root)
-	validateReference(n.Child, root)
-	validateReference(n.Bind, root)
-}
-
 func findTypeDeclare(n *mTypes.Node, targetType string) *mTypes.Node {
 	if n == nil {
 		return nil
@@ -113,24 +41,99 @@ func findTypeDeclare(n *mTypes.Node, targetType string) *mTypes.Node {
 	return nil
 }
 
-func validateExtendedTypeReference(n *mTypes.Node, root *mTypes.Node) {
+func walkNode(
+	n *mTypes.Node,
+	root *mTypes.Node,
+	used map[string]bool,
+	f func(n *mTypes.Node, root *mTypes.Node, used map[string]bool) bool,
+) {
 	if n == nil {
 		return
 	}
-	if n.IsKind(mTypes.ND_VAR_DECLARE) && n.IsType(mTypes.TY_EXTENDED) {
-		decl := findTypeDeclare(root, n.Type.ExtendName)
-		if decl == nil {
-			log.Panic("%s: undefined type: %s", error.ERROR_SYNTAX_ERROR, n.Type.ExtendName)
-		}
+
+	if !f(n, root, used) {
+		return
 	}
-	validateExtendedTypeReference(n.Next, root)
-	validateExtendedTypeReference(n.Child, root)
-	validateExtendedTypeReference(n.Bind, root)
+
+	walkNode(n.Next, root, used, f)
+	walkNode(n.Child, root, used, f)
+	walkNode(n.Bind, root, used, f)
 }
 
 func validateNode(root *mTypes.Node) {
-	validateReference(root, root)
-	validateVarDeclare(root, root, map[string]bool{})
-	validateExtendedTypeReference(root, root)
+	// To check for duplicate variable and type declarations, a Set would be appropriate,
+	// but since Go doesn't include one in the standard library,
+	// implement provisionally using a map to avoid extra dependencies.
+
+	// checks that all variable references point to valid declarations.
+	walkNode(root, root, map[string]bool{},
+		func(n *mTypes.Node, root *mTypes.Node, used map[string]bool) bool {
+			if n.IsKind(mTypes.ND_VAR_REFERENCE) {
+				decl := findDeclare(root, n.Val)
+				if decl == nil {
+					log.Panic("%s: undefined: %s", error.ERROR_SYNTAX_ERROR, n.Val)
+				}
+			}
+			return true
+		},
+	)
+	// checks for duplicate variable/type declarations and type mismatches.
+	walkNode(root, root, map[string]bool{},
+		func(n *mTypes.Node, root *mTypes.Node, used map[string]bool) bool {
+
+			if n.IsKind(mTypes.ND_TYPE_DECLARE) {
+				used[n.Val] = true
+				return false
+			}
+
+			if n.IsKind(mTypes.ND_VAR_DECLARE) {
+				if used[n.Val] {
+					log.Panic(
+						"%s: cannot use %s, already used",
+						error.ERROR_SYNTAX_ERROR,
+						n.Val,
+					)
+				}
+				used[n.Val] = true
+
+				if n.Child == nil {
+					log.Panic(
+						"%s: the value of '%s' not defined",
+						error.ERROR_SYNTAX_ERROR,
+						n.Val,
+					)
+
+				} else if n.Child.Type == nil {
+					// skip validating
+
+				} else if n.Child.IsKind(mTypes.ND_FUNCCALL) {
+					// skip validating
+
+				} else if !n.IsType(n.Child.Type.Value) {
+					log.Panic(
+						"%s: cannot use %s type as %s (%s type)",
+						error.ERROR_SYNTAX_ERROR,
+						n.Child.Type.Value,
+						n.Val,
+						n.Type.Value,
+					)
+				}
+			}
+			return true
+		},
+	)
+
+	// ensures that extended type references are defined.
+	walkNode(root, root, map[string]bool{},
+		func(n *mTypes.Node, root *mTypes.Node, used map[string]bool) bool {
+			if n.IsKind(mTypes.ND_VAR_DECLARE) && n.IsType(mTypes.TY_EXTENDED) {
+				decl := findTypeDeclare(root, n.Type.ExtendName)
+				if decl == nil {
+					log.Panic("%s: undefined type: %s", error.ERROR_SYNTAX_ERROR, n.Type.ExtendName)
+				}
+			}
+			return true
+		},
+	)
 
 }

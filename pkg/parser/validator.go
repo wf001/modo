c2481e0f7d6772314d2d1503d087293b93e8e283
wf001 <wf001@diax.xyz>
2025-05-09 16:11:23 +0900

    rename findDeclare

--- pkg/parser/validator.go
+++ pkg/parser/validator.go
@@ -6,23 +6,23 @@ import (
 	mTypes "github.com/wf001/modo/pkg/types"
 )
 
-func findDeclare(n *mTypes.Node, targetVal string) *mTypes.Node {
+func findVarDeclare(n *mTypes.Node, targetVal string) *mTypes.Node {
 	if n == nil {
 		return nil
 	}
 	if n.IsKind(mTypes.ND_VAR_DECLARE) && n.Val == targetVal {
 		return n
 	}
-	if result := findDeclare(n.Next, targetVal); result != nil {
+	if result := findVarDeclare(n.Next, targetVal); result != nil {
 		return result
 	}
-	if result := findDeclare(n.Child, targetVal); result != nil {
+	if result := findVarDeclare(n.Child, targetVal); result != nil {
 		return result
 	}
-	if result := findDeclare(n.Bind, targetVal); result != nil {
+	if result := findVarDeclare(n.Bind, targetVal); result != nil {
 		return result
 	}
-	if result := findDeclare(n.Args, targetVal); result != nil {
+	if result := findVarDeclare(n.Args, targetVal); result != nil {
 		return result
 	}
 	return nil
@@ -69,7 +69,7 @@ func validateNode(root *mTypes.Node) {
 	walkNode(root, root, map[string]bool{},
 		func(n *mTypes.Node, root *mTypes.Node, used map[string]bool) bool {
 			if n.IsKind(mTypes.ND_VAR_REFERENCE) {
-				decl := findDeclare(root, n.Val)
+				decl := findVarDeclare(root, n.Val)
 				if decl == nil {
 					log.Panic("%s: undefined: %s", error.ERROR_SYNTAX_ERROR, n.Val)
 				}
